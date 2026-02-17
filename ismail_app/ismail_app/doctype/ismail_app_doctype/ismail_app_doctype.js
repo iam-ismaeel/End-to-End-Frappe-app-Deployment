3604db73365fc9e33919a98b936dde73987e842f
iam-ismaeel <kasaliismail2@gmail.com>
2026-02-17 14:15:39 +0100

    modified doctype

---
+++ ismail_app/ismail_app/doctype/ismail_app_doctype/ismail_app_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, ismail and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ismail-app-doctype", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ismail_app/ismail_app/doctype/ismail_app_doctype/test_ismail_app_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, ismail and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testismailappdoctype(FrappeTestCase):
+	pass
